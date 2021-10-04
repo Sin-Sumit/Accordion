10cb50064558a595c618e47a633a330bd3b3a791
Oggy107 <urmalsingh10@gmail.com>
2021-10-04 20:17:18 +0530

    Made Accordion dynamic
    Merged accordion code in App.js andquestion.js file into single dynamic Accordion component

---
+++ src/Accordion.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import questions from './data';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Accordion = () => {
+    const [active, setActive] = useState(null);
+
+    const handleClick = (id) => {
+        if (active === id)
+            return setActive(null);
+        setActive(id);
+    }
+
+    return (
+        <div className="container">
+            <h3>question and answer</h3>
+            <section className="info">
+                {questions.map((question) => {
+                    return (
+                        <article className="question">
+                            <header>
+                                <h4>{question.title}</h4>
+                                <button className='btn' onClick={() => {handleClick(question.id)}}>
+                                    {active === question.id ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                                </button>
+                            </header>
+                            <p>{active === question.id && question.info}</p>
+                        </article>
+                    )
+                })}
+            </section>
+        </div>
+    )
+}
+
+export default Accordion
